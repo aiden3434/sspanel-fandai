ef30b76d1eb2fd18a5c987b71b74ce0b523102f2
aiden3434 <40372263+aiden3434@users.noreply.github.com>
2018-07-28 21:41:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
 <?php
+function ip() {
+    //strcasecmp 比较两个字符，不区分大小写。返回0，>0，<0。
+    if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+        $ip = getenv('HTTP_CLIENT_IP');
+    } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        $ip = getenv('HTTP_X_FORWARDED_FOR');
+    } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        $ip = getenv('REMOTE_ADDR');
+    } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    $res =  preg_match ( '/[\d\.]{7,15}/', $ip, $matches ) ? $matches [0] : '';
+    return $res;
+    //dump(phpinfo());//所有PHP配置信息
+}
+function getipxx($ip) {
+  $checkipurl = "http://freeapi.ipip.net/".$ip;
+  $ipxx = file_get_contents($checkipurl);
+  $find = array('[',']',']',',','"');
+  $replace = array('');
+  return str_replace($find,$replace,$ipxx);
+}
+function sqlconect($ip,$ipxx){
+  $db = new mysqli('127.0.0.1', '数据库名', '用户名', '密码');
+  if(mysqli_connect_errno()){
+    die;
+  }
+  $db-> query("insert into ip (ip,ipxx) values ('$ip','$ipxx');");  
+  mysqli_close($db);
+}
+$ip = ip();
+$ipxx = getipxx($ip);
+sqlconect($ip,$ipxx);
+?>
+<?php
 function addurl($url) {
     if((stristr($url,"https://") == FALSE) && (stristr($url,"http://") == FALSE)){
           $url = "https://".$url;
